53e5ffdbbb3c89b1b3eb239a016ad45c227aa608
Anil Maurya <anilmaurya.ui@gmail.com>
2022-06-22 11:25:09 +0530

    publish branch

---
+++ app/js/theme.js
@@ -0,0 +1,13 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// modal
+var registerAccountButton = document.getElementById('registerAccountButton');
+var registerAccountModal = new bootstrap.Modal(document.getElementById('registerAccountModal'), {
+    keyboard: false
+})
+registerAccountButton.addEventListener('click', function () {
+    registerAccountModal.toggle();
+})
